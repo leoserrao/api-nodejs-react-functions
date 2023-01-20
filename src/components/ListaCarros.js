b6702fad220bed9f938e4b11fcdbd299db4de291
Léo Serrão <leo_serrao@hotmail.com>
2023-01-20 19:54:00 -0300

    first commit

---
+++ src/components/ListaCarros.js
@@ -0,0 +1,24 @@
+import React, {useState, useEffect} from 'react'
+import axios from 'axios'
+
+export default function ListaCarros (){
+
+    const [carros, setCarros]=useState([])
+
+    useEffect(()=>{
+        axios.get('https://apinodeaula1.leoserrao.repl.co')
+        .then(res=>{
+            const dadosCarros=res.data
+            setCarros(dadosCarros)
+        })
+    })
+
+    return(
+        <div>
+            {carros.map(
+                    carro=> <div key={carro.id}>{carro.id} - {carro.marca} - {carro.modelo}</div>
+            )}
+        </div>
+    )
+}
+    
